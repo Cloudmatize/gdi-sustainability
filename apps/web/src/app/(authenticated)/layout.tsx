245f1e0b4e1fd35008c28310d35e99af3637f96c
marssaljr <marssaljr@gmail.com>
2024-11-18 18:16:06 -0300

    feat: add sidebar provider to layout

--- apps/web/src/app/(authenticated)/layout.tsx
+++ apps/web/src/app/(authenticated)/layout.tsx
@@ -1,13 +1,20 @@
-import { ReactNode } from "react";
+import { AppSidebar } from "@/components/ui/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { AuthenticatedProviders } from "@/providers/authenticated";
+import { cookies } from "next/headers";
+import type { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
 }
-export default function AuthenticatedLayout({ children }: Props) {
+export default async function AuthenticatedLayout({ children }: Props) {
+  const cookieStore = cookies()
+  const defaultOpen = cookieStore.get("sidebar:state")?.value === "true"
   return (
-    <div>
-      <AuthenticatedProviders>{children}</AuthenticatedProviders>
-    </div>
+    <SidebarProvider defaultOpen={defaultOpen}>
+      <AppSidebar />
+      <AuthenticatedProviders>
+        {children}</AuthenticatedProviders>
+    </SidebarProvider>
   );
 }
