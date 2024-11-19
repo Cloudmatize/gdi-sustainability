400a7d1328dbd5612d911c50af1015d946548343
marssaljr <marssaljr@gmail.com>
2024-11-19 11:18:01 -0300

    feat: create menu route click events

---
+++ apps/web/src/config/menuRoutes.ts
@@ -0,0 +1,46 @@
+import { Building, Bus, Cloud, Goal, Home, MapIcon } from "lucide-react";
+
+
+export const routes = [
+    {
+        id: 1,
+        title: "Visão Geral",
+        path: "#",
+        icon: Home,
+      },
+      {
+        id: 2,
+        title: "Uso e Cobertura do solo",
+        path: "/cobertura",
+        icon: MapIcon,
+      },
+      {
+        id: 3,
+        title: "Rastreador de Metas",
+        path: "#",
+        icon: Goal,
+      },
+    {
+        id: 4,
+        title: "Emissões de CO2",
+        path: "/",
+        icon: Cloud,
+        children: [
+            {
+                id: 5,
+                parent: 4,
+                title: "Emissão de Transporte",
+                path: "#",
+                icon: Bus,
+              },
+              {
+                id: 6,
+                parent: 4,
+                title: "Emissão de Edifícios",
+                path: "#",
+                icon: Building,
+              },
+        ]
+    }
+  ]
+  
