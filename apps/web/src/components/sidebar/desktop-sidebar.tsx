3e37a8cb45bc80e2b3bb83c1999e83ae4547fedc
marssaljr <marssaljr@gmail.com>
2024-11-19 19:02:35 -0300

    fix: animations and ensure to use the right components in the right place

--- apps/web/src/components/sidebar/desktop-sidebar.tsx
+++ apps/web/src/components/sidebar/desktop-sidebar.tsx
@@ -1,10 +1,11 @@
 import { routes } from "@/config/menuRoutes";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "../ui/collapsible";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "../ui/sidebar";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem, useSidebar } from "../ui/sidebar";
 import { UserMenu } from "./user-menu";
 
 export function DesktopSideBar() {
@@ -32,15 +33,18 @@ export function DesktopSideBar() {
     return (<Sidebar collapsible="icon" className="justify-center items-center">
         <SidebarHeader className="border-b py-4 gap-2 justify-between flex flex-row items-center">
             <SidebarMenu className="p-0 m-0">
-                <SidebarMenuButton asChild tooltip={open ? "Clique para esconder" : "Clique para abrir"} className="bg-transparent hover:bg-none p-0 m-0 justify-center flex flex-row" onClick={handleToggleSideBar}>
-                    <div className="flex flex-row gap-2 justify-center w-full items-center">
-                        <Image src="./icon.svg" alt="GDI Sustainability" width={24} height={24} className="rounded-md" />
-                        {open && (
-                            <p className="overflow-hidden font-medium text-lg">
+                <SidebarMenuItem>
+                    <SidebarMenuButton asChild tooltip={open ? "Clique para esconder" : "Clique para abrir"} className="bg-transparent hover:bg-none p-0 m-0 justify-center flex flex-row duration-300" onClick={handleToggleSideBar}>
+                        <div className="flex flex-row gap-2 justify-center w-full items-center">
+                            <Image src="./icon.svg" alt="GDI Sustainability" width={24} height={24} className={`${!open ? 'w-5' : 'w-5'} rounded-md `} />
+                            {/* {open && ( */}
+                            <p className={`${open ? 'text-lg' : 'text-[0px]'} font-medium transition-all ease-in-out delay-150 duration-1000 overflow-hidden`}>
                                 GDI Sustainability
                             </p>
-                        )}</div>
-                </SidebarMenuButton>
+                            {/* )} */}
+                        </div>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
             </SidebarMenu>
         </SidebarHeader>
         <SidebarContent className="py-2 flex flex-col gap-0 px-2 overflow-clip">
@@ -49,58 +53,58 @@ export function DesktopSideBar() {
                     <DropdownMenu key={route.id}>
                         <Collapsible open={openRoute.includes(route.id)} className="group/collapsible">
                             <SidebarGroup className="p-1 my-1 flex flex-col items-center">
-                                {!open ?
-                                    (<DropdownMenuTrigger>
-                                        <SidebarMenuButton tooltip={route.title} variant="default" className={`text-slate-600 ${openRoute.includes(route.id) ? "bg-teal-400 text-white w-full font-bold" : "w-full hover:bg-teal-400 hover:text-white hover:font-bold"}`} onClick={(e) => handleChangeOpenState(route)}>
-                                            <route.icon size={20} />
-                                            {open && route.title}
-                                        </SidebarMenuButton>
-                                    </DropdownMenuTrigger>
-                                    ) :
-                                    (
-                                        <SidebarGroupLabel asChild className="w-full p-0">
-                                            <CollapsibleTrigger className="w-full p-0">
-                                                <SidebarMenuButton tooltip={route.title} variant="default" className={`flex flex-row justify-between items-center text-slate-600 ${openRoute.includes(route.id) ? "bg-teal-400 text-white x w-full font-bold" : "w-full hover:bg-teal-400 hover:text-white hover:font-bold"}`} onClick={(e) => handleChangeOpenState(route)}>
-                                                    <div className="flex flex-row gap-2 items-center">
-                                                        <route.icon size={20} />
-                                                        {open && route.title}
-                                                    </div>
-                                                    {openRoute.includes(route.id) ? <ChevronUp /> : <ChevronDown />}
-
-                                                </SidebarMenuButton>
-                                            </CollapsibleTrigger>
-                                        </SidebarGroupLabel>)}
-
-                                <DropdownMenuContent className="absolute left-[2rem] bottom-[0.2rem] flex flex-col justify-start">
-                                    <DropdownMenuLabel className="flex flex-row items-center justify-start gap-2"><route.icon size={20} /> {route.title}</DropdownMenuLabel>
-                                    <DropdownMenuSeparator />
-                                    {route?.children?.map((children) => (
-                                        <DropdownMenuItem key={children.title} className="p-0 m-0 hover:bg-teal-400 max-w-56 w-56">
-                                            <SidebarMenuButton asChild variant="default" className={`max-w-56 text-slate-600 flex flex-row justify-start gap-2 ${openRoute.includes(children.id) ? "bg-teal-400 text-white w-full font-bold" : " w-full hover:bg-teal-400 hover:text-white hover:font-bold"}`}>
-                                                <a href={children.path}>
-                                                    <children.icon />
-                                                    <span>{children.title}</span>
-                                                </a>
+                                <SidebarGroupContent>
+                                    {!open ?
+                                        (<DropdownMenuTrigger>
+                                            <SidebarMenuButton tooltip={route.title} variant="default" className={`text-slate-600 font-normal ${openRoute.includes(route.id) ? "bg-teal-400 text-white w-full font-bold" : "w-full hover:bg-teal-400 hover:text-white hover:font-bold"}`} onClick={(e) => handleChangeOpenState(route)}>
+                                                <route.icon size={20} className="font-bold" />
+                                                {open && route.title}
                                             </SidebarMenuButton>
-                                        </DropdownMenuItem>
-                                    ))}
-                                </DropdownMenuContent>
-                                <CollapsibleContent>
-                                    <SidebarGroupContent className="pl-4 mt-2">
-                                        <SidebarMenu className="border-l-2 px-2">
-                                            {route?.children?.map((children) => (
-                                                <SidebarMenuItem key={children.title} className="max-w-56 w-56">
-                                                    <SidebarMenuButton asChild className={`max-w-56 text-slate-600 ${openRoute.includes(children.id) ? "bg-teal-400 text-white mb-2 w-full font-bold" : "mb-2 w-full hover:bg-teal-400 hover:text-white hover:font-bold"}`} onClick={(e) => handleChangeOpenState(children)}>
-                                                        <a href={children.path}>
-                                                            <children.icon />
-                                                            <span>{children.title}</span>
-                                                        </a>
+                                        </DropdownMenuTrigger>
+                                        ) :
+                                        (
+                                            <SidebarGroupLabel asChild className="w-full p-0">
+                                                <CollapsibleTrigger className="w-full p-0">
+                                                    <SidebarMenuButton tooltip={route.title} variant="default" className={`flex font-normal flex-row justify-between items-center text-slate-600 ${openRoute.includes(route.id) ? "bg-teal-400 text-white w-full font-bold" : "w-full hover:bg-teal-400 hover:text-white hover:font-bold"}`} onClick={(e) => handleChangeOpenState(route)}>
+                                                        <div className="flex flex-row gap-2 items-center">
+                                                            <route.icon size={20} className="font-bold" />
+                                                            {open && route.title}
+                                                        </div>
+                                                        {openRoute.includes(route.id) ? <ChevronUp /> : <ChevronDown />}
+
                                                     </SidebarMenuButton>
-                                                </SidebarMenuItem>
+                                                </CollapsibleTrigger>
+                                            </SidebarGroupLabel>)}
+
+                                    <DropdownMenuContent className="absolute left-[2rem] bottom-[0.2rem] flex flex-col gap-2 justify-start">
+                                        <DropdownMenuLabel className="flex flex-row items-center justify-start gap-2 text-slate-600"><route.icon size={20} /> {route.title}</DropdownMenuLabel>
+                                        <DropdownMenuSeparator />
+                                        {route?.children?.map((children) => (
+                                            <DropdownMenuItem key={children.title} className="p-0 m-0 hover:bg-teal-400 max-w-56 w-56 gap-2">
+                                                <SidebarMenuSubButton asChild className={`max-w-56 text-slate-600 flex flex-row justify-start gap-2 ${openRoute.includes(children.id) ? "bg-teal-400 text-white w-full font-bold" : " w-full hover:bg-teal-400 hover:text-white hover:font-bold"}`}>
+                                                    <a href={children.path}>
+                                                        <children.icon size={20} />
+                                                        <span>{children.title}</span>
+                                                    </a>
+                                                </SidebarMenuSubButton>
+                                            </DropdownMenuItem>
+                                        ))}
+                                    </DropdownMenuContent>
+                                    <CollapsibleContent>
+                                        <SidebarMenuSub className="py-2">
+                                            {route?.children?.map((children) => (
+                                                <SidebarMenuSubItem key={children.title} className="max-w-48 w-48">
+                                                    <SidebarMenuSubButton asChild className={`max-w-48 font-normal text-slate-600 ${openRoute.includes(children.id) ? "bg-teal-400 text-white mb-2 w-full font-bold" : "mb-2 w-full hover:bg-teal-400 hover:text-white hover:font-bold"}`} onClick={(e) => handleChangeOpenState(children)}>
+                                                        <Link href={children.path} className="text-slate-600">
+                                                            <children.icon size={20} className="text-slate-600" />
+                                                            <span className="duration-200">{children.title}</span>
+                                                        </Link>
+                                                    </SidebarMenuSubButton>
+                                                </SidebarMenuSubItem >
                                             ))}
-                                        </SidebarMenu>
-                                    </SidebarGroupContent>
-                                </CollapsibleContent>
+                                        </SidebarMenuSub>
+                                    </CollapsibleContent>
+                                </SidebarGroupContent>
                             </SidebarGroup>
                         </Collapsible>
                     </DropdownMenu>) : (
@@ -108,10 +112,15 @@ export function DesktopSideBar() {
                         <SidebarGroupContent>
                             <SidebarMenu>
                                 <SidebarMenuItem className="max-w-56 w-56">
-                                    <SidebarMenuButton tooltip={route.title} variant="default" className={`max-w-56 text-slate-600 ${openRoute.includes(route.id) ? "bg-teal-400 text-white w-full font-bold" : " w-full hover:bg-teal-400 hover:text-white hover:font-bold"}`} onClick={(e) => handleChangeOpenState(route)}>
-                                        <route.icon size={20} />
-                                        {open && route.title}
+                                    <SidebarMenuButton tooltip={route.title} variant="default" className={`max-w-56 font-normal text-slate-600 ${openRoute.includes(route.id) ? "bg-teal-400 text-white w-full font-bold" : " w-full hover:bg-teal-400 hover:text-white hover:font-bold"}`} onClick={(e) => handleChangeOpenState(route)}>
+                                        <Link href={route.path} className="flex flex-row gap-2">
+                                            <route.icon size={20} />
+                                            <p className={`${open ? 'text-sm' : 'text-[0px]'} transition-all ease-in-out duration-400`}>
+                                                {route.title}
+                                            </p>
+                                        </Link>
                                     </SidebarMenuButton>
+
                                 </SidebarMenuItem>
                             </SidebarMenu>
                         </SidebarGroupContent>
--- apps/web/src/config/menuRoutes.ts
+++ apps/web/src/config/menuRoutes.ts
@@ -5,41 +5,19 @@ export const routes = [
     {
         id: 1,
         title: "Visão Geral",
-        path: "#",
+        path: "/transports",
         icon: Home,
       },
       {
         id: 2,
         title: "Uso e Cobertura do solo",
-        path: "/cobertura",
+        path: "#cobertura",
         icon: MapIcon,
       },
       {
-        id: 7,
-        title: "Apenas Teste",
-        path: "/",
-        icon: Cloud,
-        children: [
-            {
-                id: 8,
-                parent: 7,
-                title: "Emissão de Transporte",
-                path: "#",
-                icon: Bus,
-              },
-              {
-                id: 9,
-                parent: 7,
-                title: "Emissão de Edifícios",
-                path: "#",
-                icon: Building,
-              },
-        ]
-    },
-      {
         id: 3,
         title: "Rastreador de Metas",
-        path: "#",
+        path: "#metas",
         icon: Goal,
       },
     {
@@ -52,14 +30,14 @@ export const routes = [
                 id: 5,
                 parent: 4,
                 title: "Emissão de Transporte",
-                path: "#",
+                path: "transports",
                 icon: Bus,
               },
               {
                 id: 6,
                 parent: 4,
                 title: "Emissão de Edifícios",
-                path: "#",
+                path: "buildings",
                 icon: Building,
               },
         ]
