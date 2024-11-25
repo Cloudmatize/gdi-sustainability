d07cfe495ccc02e473ff071310f4f0c16dc1a8aa
marssaljr <marssaljr@gmail.com>
2024-11-25 12:23:30 -0300

    fix: change card orders

--- apps/web/src/components/dashboard/page.tsx
+++ apps/web/src/components/dashboard/page.tsx
@@ -9,8 +9,8 @@ import DashboardSection12 from "./section1-2";
 export default function ComprehensiveDashboard() {
   return (
     <div className="min-h-screen bg-background p-4 md:p-6 flex flex-col gap-6 lg:p-12">
-      <DashboardSection1 />
       <DashboardSection12 />
+      <DashboardSection1 />
       <DashboardSection2 />
       <DashboardSection3 />
       <DashboardSection4 />
