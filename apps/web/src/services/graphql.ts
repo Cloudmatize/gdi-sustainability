e33db4a7760545dd76eed5270994c01b6c6ab6d2
Matheus Aragao <matheusaragaofs@gmail.com>
2024-12-02 15:13:46 -0300

    feat: update GraphQL client configuration with new API URL and add headers for improved security

--- apps/web/src/services/graphql.ts
+++ apps/web/src/services/graphql.ts
@@ -1,8 +1,10 @@
 import { GraphQLClient } from "graphql-request";
 
 export const GRAPHQL_API_URL = `${process.env.GRAPHQL_API_URL}`;
-const cubeUrl = `https://t895mffh-4000.brs.devtunnels.ms/`;
-const oldCubeUrl = `https://gds-api-dev-955707917965.us-central1.run.app/api/cube/graphql`;
-export const graphQLClient = new GraphQLClient(oldCubeUrl, {
+const cubrUrl = `https://gds-api-dev-955707917965.us-central1.run.app/api/cube/graphql`;
+export const graphQLClient = new GraphQLClient(cubrUrl, {
   errorPolicy: "ignore",
+  headers: {
+    Cm_api_key: process.env.NEXT_PUBLIC_CM_API_KEY as string
+  }
 });
