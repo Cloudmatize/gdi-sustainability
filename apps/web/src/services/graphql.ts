fd5aca40529b219e32012ae00f9898594e0ae4fd
Matheus Aragao <matheusaragaofs@gmail.com>
2024-12-02 15:15:32 -0300

    feat: update GraphQL client URL to use environment variable for improved flexibility

--- apps/web/src/services/graphql.ts
+++ apps/web/src/services/graphql.ts
@@ -1,10 +1,9 @@
 import { GraphQLClient } from "graphql-request";
 
-export const GRAPHQL_API_URL = `${process.env.GRAPHQL_API_URL}`;
-const cubrUrl = `https://gds-api-dev-955707917965.us-central1.run.app/api/cube/graphql`;
+const cubrUrl = `${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}/api/cube/graphql`;
+
 export const graphQLClient = new GraphQLClient(cubrUrl, {
-  errorPolicy: "ignore",
   headers: {
-    Cm_api_key: process.env.NEXT_PUBLIC_CM_API_KEY as string
-  }
+    Cm_api_key: process.env.NEXT_PUBLIC_CM_API_KEY as string,
+  },
 });
