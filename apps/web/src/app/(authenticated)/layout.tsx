bea960afb05f8d8cb8422b45b4190278bf8a71a7
Matheus Aragao <matheusaragaofs@gmail.com>
2024-12-02 22:12:05 -0300

    feat: wrap municipality logo in a link for improved navigation

--- apps/web/src/app/(authenticated)/layout.tsx
+++ apps/web/src/app/(authenticated)/layout.tsx
@@ -1,6 +1,7 @@
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
 import { CustomSideBarTrigger, SidebarProvider } from "@/components/ui/sidebar";
 import { AuthenticatedProviders } from "@/providers/authenticated";
+import Link from "next/link";
 import type { ReactNode } from "react";
 
 interface Props {
@@ -16,13 +17,15 @@ export default async function AuthenticatedLayout({ children }: Props) {
           <div className="w-full p-0 m-0 overflow-hidden flex flex-col gap-4">
             <div className="w-full bg-sidebar flex flex-row items-center gap-2 border-b p-2 h-[65px]">
               <CustomSideBarTrigger className="" />
-              <div className="flex items-center justify-center w-full ">
-                <img
-                  className=" h-14 p-1"
-                  src={`/logos/logo-${process.env.NEXT_PUBLIC_MUNICIPALITY_SLUG}.png`}
-                  alt={process.env.NEXT_PUBLIC_MUNICIPALITY_SLUG}
-                />
-              </div>
+              <Link href={"/"}>
+                <div className="flex items-center justify-center w-full cursor-pointer ">
+                  <img
+                    className=" h-14 p-1"
+                    src={`/logos/logo-${process.env.NEXT_PUBLIC_MUNICIPALITY_SLUG}.png`}
+                    alt={process.env.NEXT_PUBLIC_MUNICIPALITY_SLUG}
+                  />
+                </div>
+              </Link>
             </div>
             <AuthenticatedProviders>{children}</AuthenticatedProviders>
           </div>
