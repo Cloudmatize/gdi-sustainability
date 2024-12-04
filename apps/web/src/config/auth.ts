4b501194301f9816fb3cec9183ccd0f69df99c79
Matheus Aragao <matheusaragaofs@gmail.com>
2024-12-04 13:49:10 -0300

    fix: remove accessToken from session object in authOptions

--- apps/web/src/config/auth.ts
+++ apps/web/src/config/auth.ts
@@ -37,7 +37,6 @@ export const authOptions: AuthOptions = {
     async session({ token, session }: { token: JWT; session: any }) {
       return {
         ...session,
-        token: token.accessToken,
       };
     },
     async jwt({ token, account }) {
