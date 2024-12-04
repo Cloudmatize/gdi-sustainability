c335a6cee26347a857c7cb9df171a673228624ef
Matheus Aragao <matheusaragaofs@gmail.com>
2024-12-04 15:19:56 -0300

    feat: implement custom query configuration with error handling and retry logic in TanstackProvider, This configuration already had in Go Data Intelligence project, i forgot to implement here

--- apps/web/src/providers/public/tanstack.tsx
+++ apps/web/src/providers/public/tanstack.tsx
@@ -1,12 +1,59 @@
-'use client'
+"use client";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {
+  Query,
+  QueryCache,
+  QueryClientConfig,
+  QueryKey,
+} from "@tanstack/react-query";
+
+type Error = {
+  response: {
+    detail: string;
+    status: number;
+    headers: {
+      map: {
+        contentLength: string;
+        contentType: string;
+      };
+    };
+  };
+  request: {
+    query: string;
+  };
+};
 
 interface Props {
   children: React.ReactNode;
 }
+
+function QueryConfig(): QueryClientConfig {
+  const handlerOnError = (
+    error: unknown,
+    query: Query<unknown, unknown, unknown, QueryKey>
+  ) => {
+    const { response } = error as Error;
+    if (response.status === 500 && query.state.data !== undefined) {
+      query.destroy();
+    }
+  };
+
+  return {
+    queryCache: new QueryCache({
+      onError: handlerOnError,
+    }),
+    defaultOptions: {
+      queries: {
+        retry: 3,
+      },
+    },
+  };
+}
+
 export function TanstackProvider({ children }: Props) {
-  const queryClient = new QueryClient();
+  const queryConfig = QueryConfig();
+  const queryClient = new QueryClient(queryConfig);
 
   return (
     <QueryClientProvider client={queryClient}>
