e483f487add73930a43289496a97808275650e4c
marssaljr <marssaljr@gmail.com>
2024-12-11 09:30:43 -0300

    fix: remove unused code

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@cloudmatize/ts-utils"],
-  i18n: {
-    locales: ['en', 'pt'],
-    defaultLocale: 'pt',
-  }
 };
 
 export default nextConfig
