3eab45b45893045632e57648af61202aae5329b4
marssaljr <marssaljr@gmail.com>
2024-12-11 13:31:50 -0300

    feat: add translations to unauthorized access

--- apps/web/src/components/auth/unauthorized.tsx
+++ apps/web/src/components/auth/unauthorized.tsx
@@ -1,26 +1,25 @@
 import {
-  Card,
   CardContent,
   CardHeader,
-  CardTitle,
+  CardTitle
 } from "@/components/ui/card";
+import { useDictionary } from "@/context/DictionaryContext";
 import { XCircle } from "lucide-react";
 
 export default function Unauthorized() {
+  const { dict } = useDictionary()
   return (
     <div className="h-[70%] flex items-center justify-center">
       <div className="w-full max-w-md mx-auto text-center">
         <CardHeader>
           <XCircle className="w-16 h-16 text-red-500 mx-auto mb-4" />
           <CardTitle className="text-2xl font-bold text-gray-800">
-            Acesso não autorizado
+            {dict.unauthorized.title}
           </CardTitle>
         </CardHeader>
         <CardContent>
           <p className="text-gray-600">
-            Desculpe, você não tem permissão para acessar esta página. Por
-            favor, verifique suas credenciais ou entre em contato com o
-            administrador do sistema.
+            {dict.unauthorized.description}
           </p>
         </CardContent>
       </div>
