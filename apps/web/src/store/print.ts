a72d70d304b43bc0bdbef196dee0761790bf312f
Matheus Aragao <matheusaragaofs@gmail.com>
2024-12-19 10:46:09 -0300

    refactor: remove unused state properties and setters from print store for cleaner code

--- apps/web/src/store/print.ts
+++ apps/web/src/store/print.ts
@@ -3,15 +3,6 @@ import { create } from "zustand";
 interface BuildingState {
   isPrinting: boolean;
   setIsPrinting: (isPrinting: boolean) => void;
-
-  targetContent: any;
-  setTargetContent: (targetContent: any) => void;
-
-  buildingsContent: any;
-  setBuildingsContent: (buildingsContent: any) => void;
-
-  transportsContent: any;
-  setTransportsContent: (transportsContent: any) => void;
 }
 
 export const usePrintStore = create<BuildingState>()((set, get) => ({
@@ -19,23 +10,5 @@ export const usePrintStore = create<BuildingState>()((set, get) => ({
   setIsPrinting: (isPrinting) =>
     set((state) => ({
       isPrinting,
-    })),
-
-  targetContent: {},
-  buildingsContent: {},
-  transportsContent: {},
-  
-  setTargetContent: (targetContent) =>
-    set((state) => ({
-      targetContent,
-    })),
-  setBuildingsContent: (buildingsContent) =>
-    set((state) => ({
-      buildingsContent,
-    })),
-
-  setTransportsContent: (transportsContent) =>
-    set((state) => ({
-      transportsContent,
     })),
 }));
